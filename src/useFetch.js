ca394ebe7d0445598bf3552a1ba09b3ba2ef0a5c
Alex Cory <alexcory@fb.com>
2019-04-18 16:19:54 -0700

    wip

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,35 +1,85 @@
-import { useEffect, useState, useCallback } from 'react'
+import { useEffect, useLayoutEffect, useState, useCallback } from 'react'
+// import isJSON from './isJSON'
 
-export function useFetch(url, options) {
+export function useFetch(url, initialOptions) {
+  if (!url) throw Error('The URL argument is required')
   // if on server, return loading
   if (!global.window) return Object.assign([null, true, null], { data: null, loading: true, error: null })
 
   const [data, setData] = useState(null)
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState(null)
+  const [controller, setController] = useState(null) // Abortable Controller
+
+  // const isMounted = useRef(false);
+  // useLayoutEffect(() => {
+  //   isMounted.current = true;
+  //   return () => {
+  //     isMounted.current = false;
+  //   };
+  // }, []);
 
   const fetchData = useCallback(async () => {
+    if (controller !== null) {
+      controller.abort()
+    }
+
+    let options = initialOptions
+    if ('AbortController' in window) {
+      const newController = new AbortController()
+      setController(newController)
+      options = {
+        ...initialOptions,
+        signal: newController.signal
+      }
+    }
+
     try {
-      setLoading(true)
       const response = await fetch(url, options)
+      // const contentType =  response.headers.get('content-type')
       let data = null
+      // if (contentType && isJSON(contentType)) {
+      //   data = await response.json()
+      // } else {
+      //   data = await response.text()
+      // }
       try {
         data = await response.json()
-      } catch (Error) {
-        data = await response.text()
+      } finally {}
+
+      if (!data) {
+        try {
+          data = await response.text()
+        } catch (err) {
+          error = 'Invalid response format. Not JSON or text.'
+        }
       }
+
       setData(data)
       setLoading(false)
     } catch (err) {
-      setError(err)
+      const errMsg = err.name !== 'AbortError' ? err : null
+      setError(errMsg)
+      setLoading(false)
     }
-  }, [url])
+  }, [url, initialOptions])
 
   useEffect(() => {
     fetchData()
+
+    return () => {
+      if (controller !== null) {
+        controller.abort()
+      }
+    }
   }, [fetchData])
 
-  return Object.assign([data, loading, error], { data, loading, error })
+  const abort = () => controller && controller.abort()
+
+  return Object.assign(
+    [ data, loading, error, abort, fetchData ],
+    { data, loading, error, abort, refetch: fetchData }
+  )
 }
 
 export default useFetch
